f6e9b3fbd6c741686efaf2219916d3f2f4e20110
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-21 13:52:43 +0000

    fin

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -1,8 +1,9 @@
 # This method takes an array of elements (these might be strings, integers, floats, or a
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
+# I'm not sure why this is not raking...
 def find_unique_elements(arr)
-    arr_fin=[]
+   arr_fin=[]
         arr_fin<<arr.uniq
     arr_fin
 end
